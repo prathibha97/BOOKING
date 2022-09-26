02ef9b9525688b01e3dcdf2ee9aa96ec16d8e74d
Prathibha <prsthibha@gmail.com>
2022-09-26 12:37:19 +0530

    Add initial components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Hotel from "./components/hotel/Hotel";
+import List from "./components/list/List";
 
-function App (){
-    return <div>
-        <h1>Hello World!</h1>
-    </div>
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
